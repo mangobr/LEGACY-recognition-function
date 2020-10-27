3da9a69c6d1500f4efccb13c0f6b04557a06bd64
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-10-27 01:42:24 -0300

    removing custom labels fx

--- handler.py
+++ handler.py
@@ -12,10 +12,6 @@ def detect_labels(bucket, key):
     rekognition_response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
     return rekognition_response
 
-# def detect_custom_labels(bucket, key):    
-#     response = rekognition.detect_custom_labels(ProjectVersionArn='arn:aws:rekognition:us-east-1:018413147376:project/carne-moida/version/carne-moida.2020-10-09T00.57.23/1602215843140',Image={'S3Object': {'Bucket': bucket,'Name': key,}},MaxResults=10,MinConfidence=0.5)
-#     return response
-
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))

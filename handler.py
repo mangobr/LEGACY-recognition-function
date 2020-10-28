e5878e84573ac6707988ed740c52a41398791d01
Christian <christian.trindade94@gmail.com>
2020-10-28 11:02:28 -0300

    add serverless yml

--- handler.py
+++ handler.py
@@ -1,8 +1,8 @@
 import boto3
 import urllib
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
-load_dotenv()
+## load_dotenv()
 ACCESS_KEY = os.environ.get("ACCESS_KEY")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -13,8 +13,10 @@ def detect_labels(bucket, key):
     return rekognition_response
 
 def lambda_handler(event, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    # bucket = event['Records'][0]['s3']['bucket']['name']
+    # key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    bucket = 'com.rekoreko.free'
+    key = 'perfil.png'
     try:
         lambda_handler_response = detect_labels(bucket, key)
         return lambda_handler_response
